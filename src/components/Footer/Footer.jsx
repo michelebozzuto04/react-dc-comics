d6bff5598a17b0e35ed8fb9a0a88fcd70b95e1e3
michelebozzuto04 <michele.bozzuto04@gmail.com>
2025-07-14 17:58:55 +0200

    Refactor Footer component layout and styles; enhance Header styles for better visibility; add responsive grid system in index.css

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,12 +1,67 @@
 import './Footer.css'
+import '../../index.css'
 import logoBg from '../../assets/img/dc-logo-bg.png'
 
 export default function Footer() {
     return (
         <div className="footerContainer">
             <div className='container'>
-                <img className='logoBg' src={logoBg} />
+                <div className='row'>
+                    <div className='col-4'>
+                        <div className='row'>
+                            <div className='col-12'>
+                                <span className='list-title'>DC comics</span>
+                                <ul className='list'>
+                                    <li><a className='list-item' href='#'>Characters</a></li>
+                                    <li><a className='list-item' href='#'>Comics</a></li>
+                                    <li><a className='list-item' href='#'>Movies</a></li>
+                                    <li><a className='list-item' href='#'>TV</a></li>
+                                    <li><a className='list-item' href='#'>Games</a></li>
+                                    <li><a className='list-item' href='#'>Videos</a></li>
+                                    <li><a className='list-item' href='#'>News</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div className='row'>
+                            <div className='col-12'>
+                                <span className='list-title'>Shop</span>
+                                <ul className='list'>
+                                    <li><a className='list-item' href='#'>Shop DC</a></li>
+                                    <li><a className='list-item' href='#'>Shop DC Collectibles</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='col-4'>
+                        <span className='list-title'>DC</span>
+                        <ul className='list'>
+                            <li><a className='list-item' href='#'>Terms Of Use</a></li>
+                            <li><a className='list-item' href='#'>Privacy Policy (New)</a></li>
+                            <li><a className='list-item' href='#'>Ad Choices</a></li>
+                            <li><a className='list-item' href='#'>Advertising</a></li>
+                            <li><a className='list-item' href='#'>Jobs</a></li>
+                            <li><a className='list-item' href='#'>Subscriptions</a></li>
+                            <li><a className='list-item' href='#'>Talent Workshops</a></li>
+                            <li><a className='list-item' href='#'>CPSC Certifications</a></li>
+                            <li><a className='list-item' href='#'>Ratings</a></li>
+                            <li><a className='list-item' href='#'>Show Help</a></li>
+                            <li><a className='list-item' href='#'>Contact Us</a></li>
+                        </ul>
+                    </div>
+                    <div className='col-4'>
+                        <span className='list-title'>Sites</span>
+                        <ul className='list'>
+                            <li><a className='list-item' href='#'>DC</a></li>
+                            <li><a className='list-item' href='#'>MAD Magazine</a></li>
+                            <li><a className='list-item' href='#'>DC Kids</a></li>
+                            <li><a className='list-item' href='#'>DC Universe</a></li>
+                            <li><a className='list-item' href='#'>DC Power Visa</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div className='col-6'></div>
             </div>
+            {/* <img className='logoBg' src={logoBg} /> */}
         </div>
     )
 }
