dafa12ebd76226fbb8d7fd490e8b643c8db1f0ca
michelebozzuto04 <michele.bozzuto04@gmail.com>
2025-07-14 15:36:37 +0200

    Refactor App component, add Header component, and update styles; remove unused App.css

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,44 @@
+import logo from '../../assets/img/dc-logo.png'
+import './Header.css'
+
+export default function Header() {
+    return (
+        <div className='header'>
+            <div className="container">
+                <img className='logo' src={logo} />
+                <nav className='menu'>
+                    <div className='menu-link'>
+                        <a>Characters</a>
+                    </div>
+                    <div className='menu-link active'>
+                        <a>Comics</a>
+                    </div>
+                    <div className='menu-link'>
+                        <a>Movies</a>
+                    </div>
+                    <div className='menu-link'>
+                        <a>Tv</a>
+                    </div>
+                    <div className='menu-link'>
+                        <a>Games</a>
+                    </div>
+                    <div className='menu-link'>
+                        <a>Collectibles</a>
+                    </div>
+                    <div className='menu-link'>
+                        <a>Videos</a>
+                    </div>
+                    <div className='menu-link'>
+                        <a>Fans</a>
+                    </div>
+                    <div className='menu-link'>
+                        <a>News</a>
+                    </div>
+                    <div className='menu-link'>
+                        <a>Shop</a>
+                    </div>
+                </nav>
+            </div>
+        </div>
+    )
+}
