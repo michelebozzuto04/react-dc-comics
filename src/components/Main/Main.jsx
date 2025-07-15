cd16a1eda1c843b86539043e4a915bdd54eb1539
michelebozzuto04 <michele.bozzuto04@gmail.com>
2025-07-15 12:39:11 +0200

    Refactor Main component; move jumbotron styles to CSS and update structure for clarity

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -6,8 +6,8 @@ export default function Header() {
 
     return (
         <div className='mainContainer'>
+            <div className='jumbotron'></div>
             <div className='container'>
-                <div className='jumbotron'></div>
                 <span className='titleTag'>Current series</span>
                 <div className='row'>
                     {comics.map((comic) => {
